b0a197e2c2b20ea71fb3fb57fcd6d401812790a2
Mikhail Elfimov <melfimov@changers.team>
2018-04-16 20:10:20 +0300

    PB-3163 update deps, tsconfig target

--- dist/Config.js
+++ dist/Config.js
@@ -5,25 +5,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const inversify_1 = require("inversify");
 const class_validator_1 = require("class-validator");
 let Config = class Config {
-    validate() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const errors = yield class_validator_1.validate(this);
-            if (errors.length) {
-                throw new Error(`Validation failed for config ${this.getName()}, errors: ${JSON.stringify(errors)}`);
-            }
-        });
+    async validate() {
+        const errors = await class_validator_1.validate(this);
+        if (errors.length) {
+            throw new Error(`Validation failed for config ${this.getName()}, errors: ${JSON.stringify(errors)}`);
+        }
     }
     getDefaults() {
         return {};
--- dist/ConfigFactory.js
+++ dist/ConfigFactory.js
@@ -1,25 +1,15 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const class_transformer_1 = require("class-transformer");
 class ConfigFactory {
     constructor(configSource) {
         this.configSource = configSource;
     }
-    create(configConstructor) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const config = new configConstructor;
-            class_transformer_1.plainToClassFromExist(config, this.configSource.getConfig(config.getName(), config.getDefaults()));
-            yield config.validate();
-            return config;
-        });
+    async create(configConstructor) {
+        const config = new configConstructor;
+        class_transformer_1.plainToClassFromExist(config, this.configSource.getConfig(config.getName(), config.getDefaults()));
+        await config.validate();
+        return config;
     }
 }
 exports.ConfigFactory = ConfigFactory;
