d6fb68135b335059f1529878989f86590800fe3a
Mikhail Elfimov <m.elfimov@mel.fm>
2018-06-27 15:08:57 +0300

    PB-3289 redis config

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,6 +4,7 @@ import { ConfigSource } from './ConfigSource';
 import { ConfigFactory } from './ConfigFactory';
 import { DbConfig } from './DbConfig';
 import { ServerConfig } from './ServerConfig';
+import { RedisConfig } from './RedisConfig';
 import { LogConfig, LogCategoryConfig } from './LogConfig';
 import { ServicesConfig } from './ServicesConfig';
-export { ConfigFileChain, ConfigSource, Config, ConfigFactory, DbConfig, ServerConfig, LogConfig, LogCategoryConfig, ServicesConfig };
+export { ConfigFileChain, ConfigSource, Config, ConfigFactory, DbConfig, RedisConfig, ServerConfig, LogConfig, LogCategoryConfig, ServicesConfig };
